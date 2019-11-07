91ec6687b42191b07183f874ec1d6d392738cc03
Andrew Capp <acapp909@gmail.com>
2019-11-07 01:12:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,8 @@
 class Student
 
   def hello
-    puts "Hey there! I'm so excited to learn stuff."
-    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last n
-ight? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    puts "Hey there! I'm so excited to learn stuff. \nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last n
+ight? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
   
 
