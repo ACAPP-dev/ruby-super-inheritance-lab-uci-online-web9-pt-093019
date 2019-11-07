ac57d9ddc310e1bcdb2e9101f4c64f511bdfd2fa
Andrew Capp <acapp909@gmail.com>
2019-11-07 01:12:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,6 @@ class Student
     puts "Hey there! I'm so excited to learn stuff. \nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last n
 ight? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
-  
+
 
 end
